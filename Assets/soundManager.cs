41693baafe550e5ebc72cbe982dc0e0193c2b8fc
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-01-20 13:15:32 +0100

    add project

---
+++ Assets/soundManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class soundManager : MonoBehaviour
+{
+    public AudioClip[] PianoNotes;
+    public AudioSource audioSource; 
+
+
+
+    public void PlayPianoNote(int noteIndex)
+    {
+        audioSource.clip = PianoNotes[noteIndex];
+        audioSource.Play();
+
+        
+    }
+    
+}
