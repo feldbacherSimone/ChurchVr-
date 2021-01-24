36727ba4f82dff0bf6b615e4771a7e6b79195d56
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-01-24 02:47:19 +0100

    coding the piano riddle on my own because I AM A GENIOUS

--- Assets/soundManager.cs
+++ Assets/soundManager.cs
@@ -6,16 +6,54 @@ using UnityEngine.Audio;
 public class soundManager : MonoBehaviour
 {
     public AudioClip[] PianoNotes;
-    public AudioSource audioSource; 
+    public AudioSource audioSource;
 
+    [SerializeField]
+    List<int> notesPlayed;
+    [SerializeField]
+    List<int> correctSeqence;
+    int timesPlayed = 0;
 
+    public soundManager ssoundManager;
+
+  public  bool riddleIsSolved;
 
     public void PlayPianoNote(int noteIndex)
     {
         audioSource.clip = PianoNotes[noteIndex];
         audioSource.Play();
-
+        timesPlayed++;
+        notesPlayed.Add(noteIndex);
+        RightSequence(timesPlayed);
         
     }
-    
+    private void Start()
+    {
+        ssoundManager = this;
+        notesPlayed = new List<int>();
+        
+    }
+
+    void RightSequence(int inote)
+    {
+        if (timesPlayed >= correctSeqence.Count)
+        {
+            for (int i = 0; i < correctSeqence.Count; i++)
+            {
+                if (notesPlayed[i] != correctSeqence[i]) 
+                {
+                    notesPlayed.RemoveAt(0);
+                    return;
+                }
+                else
+                {
+                    riddleIsSolved = true;
+                }
+                return;
+            }
+            
+        }
+        else
+            return;
+    }
 }
